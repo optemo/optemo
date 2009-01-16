71ae2ef3d595abd54302a16820b323c913612380
Jan Ulrich <janulrich@jan-laptop.(none)>
2009-01-16 12:07:32 -0800

    Created links for webcrawlers

--- app/helpers/cameras_helper.rb
+++ app/helpers/cameras_helper.rb
@@ -1,5 +1,10 @@
 module CamerasHelper
-  
-  
-  
+  def nav_link
+    s = Session.find(session[:user_id]).last_search
+    if !s.nil?
+      link_to 'Go back to navigation', '/cameras/list/'+s 
+    else
+      link_to 'Browse more cameras', :controller => 'cameras'
+    end
+  end
 end
