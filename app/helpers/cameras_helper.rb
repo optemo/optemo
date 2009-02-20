0eed25234685ae2eb9c226098cbc61b215ecf045
Jan Ulrich <janulrich@jan-laptop.(none)>
2009-02-20 14:23:12 -0800

    Changed see more button

--- app/helpers/cameras_helper.rb
+++ app/helpers/cameras_helper.rb
@@ -27,11 +27,30 @@ module CamerasHelper
       #Clustering present
       a = @desc[i].select{|ii|ii[0]=='cluster_count'}
       count = a[0][1] if !a.nil? && !a[0].nil?
+      @desc[i].each_index do |ii|
+        item = @desc[i][ii]
+        if item[1] == 0 || item[0] == 'cluster_count'
+  		    @desc[i][ii] = nil
+  		  else
+  		    @desc[i][ii][1] = item[1]>0 ? 'high' : 'low'
+  		  end
+  		end
+  		@desc[i].compact!.each{|a|a.reverse!}
       if @search.filter
-        link_to "See more (#{count})", {:id => @cameras[i], :action => 'sim', :controller => 'search', :c => @clusters[i], :f => 1}, :class => 'sim'
+        link_to "Explore #{count} Similar Camera#{"s" if count > 1}", {:id => @cameras[i], :action => 'sim', :controller => 'search', :c => @clusters[i], :f => 1}, :title => "These cameras have " + combine_list(@desc[i])
       else
-        link_to "See more (#{count})", {:id => @cameras[i], :action => 'sim', :controller => 'search', :c => @clusters[i]}, :class => 'sim'
+        link_to "Explore #{count} Similar Camera#{"s" if count > 1}", {:id => @cameras[i], :action => 'sim', :controller => 'search', :c => @clusters[i]}, :title => "These cameras have " + combine_list(@desc[i])
       end
+    end
+  end
+  def combine_list(a)
+    case a.length
+    when 0: "similar properties to the given camera."
+    when 1: a[0].join(' ')+'.'
+    else
+      ret = "and #{a.pop.join(' ')}."
+      a.each {|i| ret = i.join(' ') + ', ' + ret }
+      ret
     end
   end
 end
