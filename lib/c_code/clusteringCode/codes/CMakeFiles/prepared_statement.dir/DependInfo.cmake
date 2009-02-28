b14eb1113d2cf31bb4ed8f738591f8f126ec465b
Maryam Mahdaviani <maryam@maryam-mahdavianis-imac.(none)>
2009-02-27 17:06:03 -0800

    ignoring CMakeFiles

--- lib/c_code/clusteringCode/codes/CMakeFiles/prepared_statement.dir/DependInfo.cmake
+++
@@ -1,8 +0,0 @@
-# The set of files whose dependency integrity should be checked:
-SET(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-SET(CMAKE_DEPENDS_CHECK_CXX
-  "/optemo/site/lib/c_code/clusteringCode/codes/prepared_statement.cpp"
-  "/optemo/site/lib/c_code/clusteringCode/codes/CMakeFiles/prepared_statement.dir/prepared_statement.o"
-  )
--- lib/c_code/clusteringCode/codes/CMakeFiles/resultset.dir/DependInfo.cmake
+++
@@ -1,8 +0,0 @@
-# The set of files whose dependency integrity should be checked:
-SET(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-SET(CMAKE_DEPENDS_CHECK_CXX
-  "/optemo/site/lib/c_code/clusteringCode/codes/resultset.cpp"
-  "/optemo/site/lib/c_code/clusteringCode/codes/CMakeFiles/resultset.dir/resultset.o"
-  )
