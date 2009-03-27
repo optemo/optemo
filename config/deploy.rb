aabf2714fab02d5b323c7be9ff74e517ac81b8d1
Jan Ulrich <janulrich@jan-laptop.(none)>
2009-03-27 14:58:05 -0700

    Created capistrano deployment

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+set :application, "optemo"
+set :repository,  "git@jaguar:optemo.git"
+set :domain, "jaguar"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+# set :deploy_to, "/var/www/#{application}"
+
+set :scm, :git
+set :deploy_via, :remote_cache
+set :user, 'jan'
+#ssh_options[:paranoid] = false
+default_run_options[:pty] = true
+set :use_sudo, false
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+############################################################
+#	Passenger
+#############################################################
+
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+after :deploy, "passenger:restart"
