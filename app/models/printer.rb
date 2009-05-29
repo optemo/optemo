d03e310053580096a9afaccdffadf0b2a1c2e8ed
Jan Ulrich <jan@jan-laptop.(none)>
2009-05-29 14:52:07 -0700

    Getting windows to play nice

--- app/models/printer.rb
+++ app/models/printer.rb
@@ -17,5 +17,9 @@ class Printer < ActiveRecord::Base
   ShowFeatures = %w(brand model ppm paperinput ttp resolution itemwidth itemheight itemlength duplex connectivity papersize scanner printserver platform)
   ShowFeaturesDisp = %w(Brand Model Pages\ Per\ Minute Paper\ Tray\ Size Time\ To\ Print Resolution Width Height Length Duplex Connectivity Paper\ Size Scanner Print\ Server OS)
   InterestingFeatures = %w(brand ppm ttp resolution duplex connectivity papersize paperoutput dimensions dutycycle paperinput ppmcolor platform colorprinter scanner printserver itemheight itemlength itemwidth itemweight manufacturer model packageheight packagelength packagewidth packageweight)
+  
+  def valid?
+    instock && !(ppm.nil? || itemwidth.nil? || paperinput.nil? || salepriceint.nil? || resolutionarea.nil? || scanner.nil? || printserver.nil?)
+  end
 end
 
