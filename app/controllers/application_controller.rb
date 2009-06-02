afc15332d8c233bbf98a8cf0479d43433e14eb04
Jan Ulrich <jan@jan-laptop.vancouver.sfu.ca>
2009-06-02 10:43:28 -0700

    Added multi-stage deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,5 +56,4 @@ class ApplicationController < ActionController::Base
     end
     s
   end
-  
 end
---
+++ config/deploy/production.rb
@@ -0,0 +1,49 @@
+set :application, "optemo_site"
+set :repository,  "git@jaguar:site.git"
+set :domain, "jaguar"
+set :branch, "staging"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+# set :deploy_to, "/var/www/#{application}"
+
+set :scm, :git
+set :deploy_via, :remote_cache
+#ssh_options[:paranoid] = false
+default_run_options[:pty] = true
+set :use_sudo, false
+
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+############################################################
+#	Passenger
+#############################################################
+
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+desc "Compile C-Code"
+task :compilec do
+  run "cp -rf #{current_path}/lib/c_code/clusteringCodeLinux/* #{current_path}/lib/c_code/clusteringCode"
+  run "cd #{current_path}/lib/c_code/clusteringCode/ && make clean && make connect"
+end
+
+desc "Configure the server files"
+task :serversetup do
+  # Instantiate the database.yml file
+  run "cd #{current_path}/config              && cp -f database.yml.deploy database.yml"
+end
+
+after :deploy, "serversetup"
+after :serversetup, "compilec"
+after :compilec, "passenger:restart"
+
+
