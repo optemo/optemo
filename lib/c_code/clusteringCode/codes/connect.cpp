e389051bd209168a2bc32f9fe10665f33a8d6e1c
Maryam <maryam@maryam-mahdavianis-imac.vancouver.sfu.ca>
2009-06-03 11:52:09 -0700

    changed the database.yml path in connect.cpp

--- lib/c_code/clusteringCode/codes/connect.cpp
+++ lib/c_code/clusteringCode/codes/connect.cpp
@@ -240,7 +240,7 @@ int *mergedClusterN= new int[clusterN];
 		vector<string> tokens;
 		ifstream myfile;
 		int i=0;
-	   myfile.open("/site/config/database.yml"); 
+	   myfile.open("/optemo/site/config/database.yml"); 
 	   if (myfile.is_open()){
 		while (! myfile.eof()){
 			getline (myfile,line);
