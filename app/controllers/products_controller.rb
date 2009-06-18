919c92bf2e10b190bfc1078c0e4ba85b372a6e42
Sunjeet Singh <sunjeetsingh@sunjeet-singhs-macbook.vancouver.sfu.ca>
2009-06-18 10:54:17 -0700

    Function for calculating factor (for a particular product and feature) added to controller.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,9 +27,22 @@ class ProductsController < ApplicationController
     @dbfeat = {}
     DbFeature.find_all_by_product_type(@pt).each {|f| @dbfeat[f.name] = f}
     #Previously clicked product
+<<<<<<< HEAD:app/controllers/products_controller.rb
     @searches = [Search.find_by_session_id(@session.id, :order => 'updated_at desc')]
     @s = Search.searchFromPath(params[:path_info], @session)
     @picked_products = @session.saveds.map {|s| $model.find(s.product_id)}
+=======
+    #@searches = []
+    #if session[:search_id]
+    #  @search = Search.find(session[:search_id]) 
+    #  currentsearch = @search
+    #  while (!currentsearch.parent_id.nil?) do
+    #    @searches<<currentsearch.id
+    #     currentsearch = Search.find(currentsearch.parent_id)
+    #  end
+    #end
+        
+>>>>>>> Function for calculating factor (for a particular product and feature) added to controller.:app/controllers/products_controller.rb
   end
 
   # GET /products/1
