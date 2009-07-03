4f5d8efb39bf3701e4f23f64781a8ba99a352b51
Sunjeet Singh <sunjeetsingh@sunjeet-singhs-macbook.vancouver.sfu.ca>
2009-07-03 15:56:33 -0700

    preference_relations weights updated instead of adding rows for existing relations, and preference_relations table truncated upon creation of new session

--- app/models/preference_relation.rb
+++ app/models/preference_relation.rb
@@ -7,9 +7,15 @@ class PreferenceRelation < ActiveRecord::Base
      relation [:higher] = higher
      relation[:lower] = lower
      relation[:session_id] = sessionId
-     relation[:weight] = weight
-     newRelation = PreferenceRelation.new(relation)
+     relation[:weight] = 0  # Keep weight 0 initially. Then if record is found or new record is created, add weight
+     newRelation = PreferenceRelation.find_or_create_by_higher_and_lower(relation)
+     newRelation.weight = newRelation.weight + weight
      newRelation.save
+   end
+   
+   def self.deleteBinaryRelations
+      # PreferenceRelation.delete_all() # Delete does not reset ID field, so use TRUNCATE
+      ActiveRecord::Base.connection.execute('TRUNCATE preference_relations')
    end
    
 end
