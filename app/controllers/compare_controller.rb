87eaae0170b187f7e2c1e48b6dd364913a56f0a0
Sunjeet Singh <sunjeetsingh@sunjeet-singhs-macbook.(none)>
2009-07-07 17:31:17 -0700

    Working on Product evaluator, fading out similar terms. EOD Tueday

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -13,8 +13,11 @@ class CompareController < ApplicationController
     # Link to latest product navigation page
     @navigationUrl = "/printers/list/" + Search.find_all_by_session_id(session[:user_id], :order => 'updated_at DESC').first.to_s
     # To track whether an interesting feature is displayed or not-
-    @interestingFeatureDisplayed = {} # Array.new(session[:productType].constantize::DisplayedFeatures.count, false)
+    @interestingFeatureDisplayed = {} 
+    @featureCssClass = {}
     @saveds = Saved.find_all_by_session_id(session[:user_id])
+    @dbfeat = {}
+    DbFeature.find_all_by_product_type(session[:productType]).each {|f| @dbfeat[f.name] = f}
     @saveds.collect do |saved|
       @products << session[:productType].constantize.find(saved.product_id)
     end
@@ -139,6 +142,8 @@ def decideWhichFeaturesToDisplay
 			if canShowFeature?(column, i)
 				@interestingFeatureDisplayed[column] = true   # If once set, the feature dispaly will be true for all products
 			end
+			# @featureCssClass[column] = "lowlightfeature"
+			# @featureCssClass[column] = "regularfeature"
 		end
   end
 end
@@ -186,12 +191,42 @@ def featuresDictionary
     else 
       return displayString.capitalize 
     end
-=begin
-    when 'itemwidth', 'itemlength', 'itemheight', 'itemweight': 
-    	return displayString[0..3].capitalize + ' ' + displayString[4..-1] 
-    when "packagewidth", "packagelength", "packageheight", "packageweight":
-    	return  displayString[0..6].capitalize + ' ' + displayString[7..-1] 
-=end
+end
+
+def DispContFeatureDictionary(f)
+  case f
+    when "resolution"
+      return "resolutionmax"
+    when "ppmcolor"
+      return "ppm"
+    else 
+      return f 
+    end
+end
+
+def finalDisplay(product, column)
+  case column
+    when 'brand'
+      return product.display(column) + ' ' + product.display('model') 
+    when 'itemdimensions'
+      return product.display('itemlength') + " x " + product.display('itemwidth') + " x " + product.display('itemheight') 
+    when 'packagedimensions'
+    	return product.display('packagelength') + " x " + product.display('packagewidth') + " x " + product.display('packageheight')
+    when 'price'
+      return product.display('pricestr')
+    else
+      case product.display(column)
+        # Display Unavailable instead of Unknown
+        when 'Unknown' 
+    	    return "Unavailable"
+        when "true"
+          return image_tag '/images/checkmark.png', :width => 18, :height => 18
+        when "false"
+    	    return "x"		
+        else
+          return product.display(column)[0..20]
+        end
+    end
 end
 
 def ReorderProducts
@@ -213,14 +248,45 @@ def ReorderFeatures
   
   prefHash = {}
   session[:productType].constantize::DisplayedFeatures.each do |f|
-    if session[:productType].constantize::ContinuousFeatures.index(f) == nil
+    correspondingf = DispContFeatureDictionary(f)
+    if session[:productType].constantize::ContinuousFeatures.index(correspondingf) == nil
       prefHash[f] = 0
     else
-      prefHash[f] = @session.features.send((f + "_pref").intern)
+      prefHash[f] = @session.features.send((correspondingf + "_pref").intern)
     end
   end
   # Reorder features in DisplayedFeatures into @preferredDisplayFeatures 
   @preferredDisplayFeatures = prefHash.sort{|a,b| a[1]<=>b[1]}.reverse  
+end
+
+def sameFeatureValue(f)
+  for i in 0..@products.count-2
+    # if finalDisplay(@products[i], f) != finalDisplay(@products[i+1], f)
+    if notSimilarFeatures(f, finalDisplay(@products[i], f), finalDisplay(@products[i+1], f))
+      return false
+    end
+  end
+  return true
+end
+
+def notSimilarFeatures(f, f1, f2)
+# Definition of similarity, for different features can be provided here
+debugger
+  case f
+    when "scanner" || "printserver" || "colorprinter"
+      if (f1 == f2)
+        return false
+      end
+    when "ppm" || "papersize" || "itemweight" || "paperoutput" || "packageweight" # f in continuous features???
+      margin = @dbfeat[f].max - @dbfeat[f].min
+      margin = margin/10
+      if (f1.to_f - f2.to_f).abs <= margin 
+        return false
+      end
+      return true
+    else
+      return true
+    end
 end
 
 def SortArray
