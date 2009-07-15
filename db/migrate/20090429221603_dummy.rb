f648bc3f47a4b616ffcb2383c1cbe4d14313a890
Jan Ulrich <jan@jan-laptop.vancouver.sfu.ca>
2009-07-15 16:34:36 -0700

    Download reviews

--- db/migrate/20090429221603_dummy.rb
+++ db/migrate/20090429221603_dummy.rb
@@ -2,6 +2,12 @@ class Dummy < ActiveRecord::Migration
   def self.up
     #add_column :amazon_printers, :product_id, :integer
     #add_column :printers, :manufacturerproducturl, :string
+    add_column :printers, :averagereviewrating, :float
+    add_column :printers, :totalreviews, :integer
+    add_column :cameras, :averagereviewrating, :float
+    add_column :cameras, :totalreviews, :integer
+    add_column :amazon_printers, :averagereviewrating, :float
+    add_column :amazon_printers, :totalreviews, :integer
   end
 
   def self.down
