5dcb6d9ef73997b1a2d86ed5644b9a28e2dd74ce
Jan Ulrich <jan@jan-ulrichs-macbook.local>
2009-07-22 17:58:09 -0700

    Fixed camera stuff

--- app/models/camera_features.rb
+++ app/models/camera_features.rb
@@ -1,2 +1,11 @@
 class CameraFeatures < ActiveRecord::Base
+  
+  def commit(sessionId)
+    oldfeatures = CameraFeatures.find(:first, :conditions => ['session_id = ?', sessionId])
+    atts = attributes
+    # Remove all nil fields from atts except if their BinaryFeatures as we're tracking only positive check boxes
+    atts.delete_if{|k,v|v.blank? && ($model::BinaryFeatures.empty? || !k.index(/#{$model::BinaryFeatures.join('|')}/))}
+    oldfeatures.update_attributes(atts)
+  end
+  
 end
