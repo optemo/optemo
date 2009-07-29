8f0385b554fe684fc800a3c4d8c13f43dbf201a7
Maryam <maryam@maryam-mahdavianis-imac.vancouver.sfu.ca>
2009-07-29 09:39:48 -0700

    commenting the distributions method

--- app/models/search.rb
+++ app/models/search.rb
@@ -30,32 +30,31 @@ class Search < ActiveRecord::Base
     round2Decim(normalize(res))
   end
   
-  def distributions
-      dists = {}
-        debugger
-      dbfeat = DbFeature.find_all_by_product_type_and_feature_type(session.product_type, 'Continuous')
-      acceptedNodes = clusters.map{|c| c.nodes(session)}.flatten ###??? more efficent way?
-      dbfeat.each do |f|
-        # intializing 
-        #dists{f.name} = Array.new(10,0) 
-        stepsize = (f.max-f.min).to_f/10 
-        acceptedNodes.each do |n| 
-          10.time do |i| 
-              min = f.min + stepsize * i
-              max = min + stepsize
-              if (max == min)
-                dists{f.name}[i] += 1 if n.send(f.name) == min
+#def distributions
+#    dists = {}
+#     
+#    dbfeat = DbFeature.find_all_by_product_type_and_feature_type(session.product_type, 'Continuous')
+#    acceptedNodes = clusters.map{|c| c.nodes(session)}.flatten ###??? more efficent way?
+#    dbfeat.each do |f|
+#      # intializing 
+#      #dists{f.name} = Array.new(10,0) 
+#      stepsize = (f.max-f.min).to_f/10 
+#      acceptedNodes.each do |n| 
+#        10.times do |i| 
+#            min = f.min + stepsize * i
+#            max = min + stepsize
+#            if (max == min)
+#              dists[f.name][i] += 1 if n.send(f.name) == min
+#
+#            elsif (n.send(f.name)>=min && n.send(f.name) < max)
+#              debugger
+#              dists[f.name][i] += 1 
+#            end
+#        end
+#      end      
+#   end    
+#end 
 
-              elsif (n.send(f.name)>=min && n.send(f.name) < max)
-                dists{f.name}[i] += 1 
-              end
-          end
-        end      
-     end    
-  end 
-  
-  
-  
   
   #Range of product offerings
   def ranges(featureName)
