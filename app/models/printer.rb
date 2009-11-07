842693b74cd36829c0cca112d8e5781722db153c
Jan Ulrich <jan@jan-laptop.(none)>
2009-11-06 17:24:44 -0800

    Layout changes

--- app/models/printer.rb
+++ app/models/printer.rb
@@ -6,15 +6,16 @@ class Printer < ActiveRecord::Base
            #                                (c)luster 
            #                                (f)ilter 
            #                                (d)escription
+           # =>                             (x)compared
            #     db_name           Type     (e)xtra Display
-  Features = [%w(ppm               Continuous  cfd  ),
-              %w(itemwidth         Continuous  cfd  ),
-              %w(paperinput        Continuous  cfd  ),
-              %w(resolutionmax     Continuous  cfd  ),
-              %w(price             Continuous  cfd  ),
-              %w(brand             Categorical f    ),
-              %w(scanner           Binary      cf   ),
-              %w(printserver       Binary      cf   )]
+  Features = [%w(price             Continuous  xcfd  ),
+              %w(ppm               Continuous  xcfd  ),
+              %w(itemwidth         Continuous  xcfd  ),
+              %w(paperinput        Continuous  xcfd  ),
+              %w(resolutionmax     Continuous  xcfd  ),
+              %w(brand             Categorical xf    ),
+              %w(scanner           Binary      xcf   ),
+              %w(printserver       Binary      xcf   )]
                                                    
   ContinuousFeatures = Features.select{|f|f[1] == "Continuous" && f[2].index("c")}.map{|f|f[0]}
   DescFeatures = Features.select{|f|f[2].index("d")}.map{|f|f[0]}
@@ -25,7 +26,7 @@ class Printer < ActiveRecord::Base
   CategoricalFeaturesF = Features.select{|f|f[1] == "Categorical" && f[2].index("f")}.map{|f|f[0]}
   ExtraFeature = Hash[*Features.select{|f|f[2].index("e")}.map{|f|[f[0],true]}.flatten]
   ShowFeatures = %w(brand model ppm paperinput ttp resolution itemwidth itemheight itemlength duplex connectivity papersize scanner printserver platform)
-  #DisplayedFeatures = %w(displaysize opticalzoom maximumresolution itemweight itemdimensions digitalzoom)
+  DisplayedFeatures = Features.select{|f|f[2].index("x")}.map{|f|f[0]}
   ItoF = %w(price itemwidth)
   named_scope :priced, :conditions => "price > 0"
   named_scope :valid, :conditions => [ContinuousFeatures.select{|f|!f.match(/^minf|maximumfocallength|minimumfocallength$/)}.map{|i|i+' > 0'}.join(' AND '),BinaryFeatures.map{|i|i+' IS NOT NULL'}.join(' AND '),['brand','model'].map{|i|i+' IS NOT NULL'}.join(' AND ')].delete_if{|l|l.blank?}.join(' AND ')
