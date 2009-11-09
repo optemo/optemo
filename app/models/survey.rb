060351f999c5a892a6ecb550c57f7dd580d41259
Jan Ulrich <jan@jan-laptop.(none)>
2009-11-09 14:06:01 -0800

    Survey is fixed and titlebar

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,6 @@
 class Survey < ActiveRecord::Base
 
-TotalQuestions = 3
-SurveyQuestionCount = 3   # number of questions to be asked in survey
+TotalQuestions = 1
+SurveyQuestionCount = 1   # number of questions to be asked in survey
 
 end
