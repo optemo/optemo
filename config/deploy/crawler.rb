84be81ee457526d623c96ea17e8afa081dffb893
Zev Thompson <zev@optemo.com>
2010-01-18 15:03:06 -0800

    New search method for low numbers of results, much faster. Comments regarding umask and capistrano have been added to recipes as a way of documenting that ongoing struggle.

--- config/deploy/crawler.rb
+++ config/deploy/crawler.rb
@@ -15,6 +15,9 @@ set :scm, :git
 set :deploy_via, :remote_cache
 #ssh_options[:paranoid] = false
 default_run_options[:pty] = true
+# The above command allows for interactive commands like entering ssh passwords, but
+# the problem is that "umask = 002" is getting ignored, since .profile isn't being sourced.
+# :pty => true enables for a given command if we set the above to false eventually
 set :use_sudo, false
 
 role :app, domain
