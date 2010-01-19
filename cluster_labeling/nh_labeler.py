a3587734222735123e5d961e4a0e8640652abc92
Nimalan Mahendran <nimalan@cs.ubc.ca>
2010-01-18 21:21:35 -0800

    Changed filename for sqlite db since its more than just wordcounts now

--- cluster_labeling/nh_labeler.py
+++ cluster_labeling/nh_labeler.py
@@ -15,9 +15,9 @@ try:
 except(RuntimeError):
     pass
 
-wordcount_filename = '/optemo/site/cluster_hierarchy_wordcounts'
 import cluster_labeling.optemo_django_models as optemo
 
+wordcount_filename = '/optemo/site/cluster_hierarchy_counts'
 db = sqlite3.connect(wordcount_filename)
 
 # Create table
