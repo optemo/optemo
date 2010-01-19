8cdce849025b0a8bf0a9a83b67b0d129362bb4e8
Jan Ulrich <jan@jan-laptop.(none)>
2010-01-19 13:41:58 -0800

    Mistake in changing names

--- app/models/cartridge.rb
+++ app/models/cartridge.rb
@@ -4,7 +4,7 @@ class Cartridge < ActiveRecord::Base
   CategoricalFeatures = %w(color)
   ContinuousFeatures =  %w(yield)
   named_scope :priced, :conditions => "price > 0"
-  named_scope :valid, :conditions => [ContinuousFeatures.map{|i|i+' > 0'}.join(' AND '),CategoricalFeaturesF.map{|i|i+' IS NOT NULL'}.join(' AND ')].delete_if{|l|l.blank?}.join(' AND ')
+  named_scope :valid, :conditions => [ContinuousFeatures.map{|i|i+' > 0'}.join(' AND '),CategoricalFeatures.map{|i|i+' IS NOT NULL'}.join(' AND ')].delete_if{|l|l.blank?}.join(' AND ')
   named_scope :instock, :conditions => "instock is true"
   named_scope :instock_ca, :conditions => "instock_ca is true"
   named_scope :identifiable, :conditions => [IDFeatures.map{|i|i+' IS NOT NULL'}.join(' AND ')].delete_if{|l|l.blank?}.join(' AND ')
