1832cc3ba165ee98e0029f2295d57976487a04a4
Nimalan Mahendran <nimalan@cs.ubc.ca>
2010-01-25 15:27:16 -0800

    Djangoified cluster total counts tables

--- cluster_labeling/cluster_totalcounts_table.py
+++ cluster_labeling/cluster_totalcounts_table.py
@@ -1,109 +1,50 @@
 #!/usr/bin/env python
-import sqlite3
+from django.db import models
+from django.db import connections, transaction
 
-class ClusterTotalCountTable():
-    table_cols = \
-    {
-    "cluster_id" : "integer",
-    "parent_cluster_id" : "integer",
-    "totalcount" : "integer",
-    "numchildren" : "integer"
-    }
+from django.db.models import Sum
 
-    tablename = None
+import cluster_labeling.local_django_models as local
 
-    def __init__(self, tablename):
-        self.tablename = tablename
+class ClusterTotalCount(local.LocalInsertOnlyModel):
+    class Meta:
+        abstract = True
+        unique_together = (("cluster_id"))
 
-    def gen_create_totalcount_table_sql(self):
-        return \
-        "CREATE TABLE " + self.tablename + " " + \
-        "(" + \
-        ', '.join(map(lambda (k,v): ' '.join([k,v]),
-                      self.table_cols.iteritems())) + \
-        ", PRIMARY KEY (cluster_id) " + \
-        "CONSTRAINT totalcount_check CHECK (totalcount > 0) " + \
-        "CONSTRAINT numchildren_check CHECK (numchildren >= 0)" + \
-        ")"
+    cluster_id = models.BigIntegerField(primary_key=True)
+    parent_cluster_id = models.BigIntegerField()
+    totalcount = models.BigIntegerField()
+    numchildren = models.IntegerField()
 
-    def create_totalcount_table(self, db):
-        c = db.cursor()
-        c.execute(self.gen_create_totalcount_table_sql())
-        db.commit()
-        c.close()
-
-    def drop_totalcount_table(self, db):
-        c = db.cursor()
-        c.execute("DROP TABLE IF EXISTS " + self.tablename)
-        db.commit()
-        c.close()
-
-    def gen_insert_totalcount_entry_sql(self):
-        return \
-        "INSERT INTO " + self.tablename + \
-        "(cluster_id, parent_cluster_id, numchildren, totalcount) " + \
-        "VALUES (?, ?, ?, ?)"
-
-    def add_totalcount_entry(self, db, cluster_id, parent_cluster_id,
-                             numchildren, totalcount):
-        c = db.cursor()
-
-        try:
-            c.execute(self.gen_insert_totalcount_entry_sql(),
-                      (cluster_id, parent_cluster_id,
-                       numchildren, totalcount))
-            db.commit()
-            c.close()
-        except sqlite3.IntegrityError:
-            print "Integrity error: cluster_id == %d" % \
-                  (cluster_id,)
-
-            import pdb
-            pdb.set_trace()
-
-            raise
-
-    def gen_select_totalcount_entry_sql(self):
-        return \
-        "SELECT totalcount from " + self.tablename + \
-        " WHERE cluster_id = ?"
-
-    def get_totalcount(self, db, cluster_id):
-        c = db.cursor()
-        c.execute(self.gen_select_totalcount_entry_sql(),
-                  (cluster_id,))
-        results = c.fetchall()
-        c.close()
-
-        totalcount = None
-
-        if len(results) > 0:
-            totalcount = results[0][0]
-
-        return totalcount
-
-    def gen_sum_child_totalcounts_sql(self):
-        return \
-        "SELECT SUM(totalcount) from " + self.tablename + " " + \
-        "WHERE parent_cluster_id = ?"
-
+    @classmethod
     def sum_child_cluster_totalcounts\
-            (self, db, cluster_id, parent_cluster_id, numchildren):
-        c = db.cursor()
-        c.execute(self.gen_sum_child_totalcounts_sql(), (cluster_id,))
+        (cls, cluster_id, parent_cluster_id, numchildren):
+        qs = cls.objects.\
+             filter(parent_cluster_id = cluster_id).\
+             aggregate(totalcount_sum=Sum('totalcount'))
 
-        while (1):
-            row = c.fetchone()
-            if row == None:
-                break
+        totalcount_sum = qs['totalcount_sum']
 
-            totalcountsum = row[0]
+        if totalcount_sum == None:
+            # All of the cluster's children have zero entries. Zero
+            # entries aren't stored in the table.
+            return
 
-            if totalcountsum == 0:
-                continue
+        cluster_totalcount = \
+            cls(cluster_id=cluster_id,
+                parent_cluster_id=parent_cluster_id,
+                totalcount=totalcount_sum,
+                numchildren=numchildren)
+        cluster_totalcount.save()
 
-            self.add_totalcount_entry\
-            (db, cluster_id, parent_cluster_id,
-             numchildren, totalcountsum)
+class ClusterWordTotalCount(ClusterTotalCount):
+    class Meta:
+        db_table = 'wordtotalcounts'
 
-        c.close()
+class ClusterProdTotalCount(ClusterTotalCount):
+    class Meta:
+        db_table = 'prodtotalcounts'
+
+class ClusterReviewTotalCount(ClusterTotalCount):
+    class Meta:
+        db_table = 'reviewtotalcounts'
