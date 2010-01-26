21623e56bf8d3fb6c277db2039be9c6a9da4bb36
Nimalan Mahendran <nimalan@cs.ubc.ca>
2010-01-25 18:16:51 -0800

    Adding cluster score table class

---
+++ cluster_labeling/cluster_score_table.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from django.db import models
+import cluster_labeling.cluster_value_for_word_table as cvfwt
+
+class ClusterScore(cvfwt.ClusterValueForWord):
+    class Meta:
+        abstract = True
+        unique_together = (("cluster_id", "word"))
+
+    value_name = "score"
+    score = models.BigIntegerField()
