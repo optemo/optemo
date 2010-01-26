b0501bfcc7a8eaca9893063d0bccdbd598eee33b
Nimalan Mahendran <nimalan@cs.ubc.ca>
2010-01-25 18:17:10 -0800

    Adding a score table for mutual information

--- cluster_labeling/cluster_score_table.py
+++ cluster_labeling/cluster_score_table.py
@@ -9,3 +9,8 @@ class ClusterScore(cvfwt.ClusterValueForWord):
 
     value_name = "score"
     score = models.BigIntegerField()
+
+class ClusterMIScore(ClusterScore):
+    class Meta:
+        db_table = 'mi_scores'
+        unique_together = (("cluster_id", "word"))
