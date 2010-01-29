467c3e4ca143169d6975534808c319a7403ae1c4
nimalan <nimalan@jaguar.vc.shawcable.net>
2010-01-28 16:19:32 -0800

    Changing word count scoring code to use numpy instead of flat lists
    - N[u, c] is now a 2 x 2 numpy array instead of a flat list, making indexing
      and summing easier and (probably) faster.

--- cluster_labeling/score_utils.py
+++ cluster_labeling/score_utils.py
@@ -2,6 +2,8 @@
 import cluster_labeling.cluster_count_table as cct
 import cluster_labeling.cluster_totalcount_table as ctct
 
+from numpy import *
+
 def bin_to_int(idx):
     return int(idx, 2)
 
@@ -9,29 +11,21 @@ def get_N_UC(cluster_id, word):
     count_table = cct.ClusterReviewCount
     totalcount_table = ctct.ClusterReviewTotalCount
 
-    N = [0, 0, 0, 0]
+    N = zeros((2, 2))
 
-    N[bin_to_int('11')] = count_table.get_value(cluster_id, word)
-    N[bin_to_int('10')] = count_table.get_value(0, word) - N_11
-    N[bin_to_int('01')] = \
-        totalcount_table.get_value(cluster.id) - N_11
-    N[bin_to_int('00')] = \
-        totalcount_table.get_value(0) - N_01 - N_10 - N11
+    N[1, 1] = count_table.get_value(cluster_id, word)
+    N[1, 0] = count_table.get_value(0, word) - N[1, 1]
+    N[0, 1] = totalcount_table.get_value(cluster_id) - N[1, 1]
+    N[0, 0] = totalcount_table.get_value(0) - N[0, 1] - N[1, 0] - N[1, 1]
 
     return N
 
 def P_UC_to_P_C(P_UC):
-    P_C = map(lambda probs: reduce(lambda x, y: x + y, probs),
-              map(lambda vars: map(lambda var: P_UC[bin_to_int(var)],
-                                   vars),
-                  [['11', '01'], ['10', '00']]))
+    P_C = sum(P_UC, 0)
     return P_C
 
 def P_UC_to_P_U(P_UC):
-    P_U = map(lambda probs: reduce(lambda x, y: x + y, probs),
-              map(lambda vars: map(lambda var: P_UC[bin_to_int(var)],
-                                   vars),
-                  [['11', '10'], ['01', '00']]))
+    P_U = sum(P_UC, 1)
     return P_U
 
 def compute_scores_for_cluster(cluster_id, score_fn, score_table):
