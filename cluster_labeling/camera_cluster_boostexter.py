f023500ba2fb59af8efff295e85db19394a03d15
Nimalan Mahendran <nimalan@cs.ubc.ca>
2010-02-11 17:52:02 -0800

    Added a function for running boostexter on all of the clusters

--- cluster_labeling/camera_cluster_boostexter.py
+++ cluster_labeling/camera_cluster_boostexter.py
@@ -147,3 +147,12 @@ def train_boostexter(cluster):
 
 def get_boostexter_rules():
     pass
+
+def train_boostexter_on_all_clusters\
+        (version = optemo.CameraCluster.get_latest_version()):
+    qs = optemo.CameraCluster.get_manager().filter(version=version)
+
+    for cluster in qs:
+        generate_names_file(cluster)
+        generate_data_file(cluster)
+        train_boostexter(cluster)
