4fa6a75d74bc56c8e135c538d999330176097b57
Nimalan Mahendran <nimalan@cs.ubc.ca>
2010-03-02 14:40:59 -0800

    Replacing global stems with a table of stem labels
    - All that is really needed is a mapping from a stem (i.e. 'wordlessli') to a
      stem label (i.e. 'wordlessly'), for a given stemmer. This still allows one to
      check that two words stem to the same thing, while also storing away a label
      for the stem. The label is seleted as the shortest word that stems to a
      particular stem.

--- cluster_labeling/stemmer.py
+++ cluster_labeling/stemmer.py
@@ -10,51 +10,50 @@ import cluster_labeling.local_django_models as local
 from django.db import models
 from django.db import transaction
 
-class GlobalStem(local.LocalModel):
+class StemLabel(local.LocalModel):
     class Meta:
-        db_table='global_stems'
+        db_table='stem_labels'
     
-    word = models.CharField(max_length=255, unique=True)
-    stem = models.CharField(max_length=255)
+    label = models.CharField(max_length=255, unique=True)
+    stem = models.CharField(max_length=255, unique=True)
 
-def build_global_stem_table\
+def build_stem_label_table\
         (version=optemo.CameraCluster.get_latest_version()):
-    GlobalStem.drop_table_if_exists()
-    GlobalStem.create_table()
+    StemLabel.drop_table_if_exists()
+    StemLabel.create_table()
 
-    # Compute all stems
+    # Compute all stem labels
     qs = optemo.Review.get_manager().all()
     for review in qs:
-        compute_global_stems_for_review(review)
-
-    # Find the shortest word for each stem and make it the stem
-    stem_qs = GlobalStem.get_manager().values('stem').distinct()
-    for result in stem_qs:
-        stem = result['stem']
-
-        shortest_word = GlobalStem.get_manager().filter(stem=stem)
-        shortest_word = \
-            shortest_word.extra(select={'word_len':'length(word)'},
-                                order_by=['word_len'])[0].word
-
-        GlobalStem.get_manager().filter(stem=stem).update(stem=shortest_word)
+        compute_stem_labels_for_review(review)
 
 @transaction.commit_on_success
-def compute_global_stems_for_review(review):
+def compute_stem_labels_for_review(review):
     content = review.content
     words = nh.get_words_from_review(content)
 
     for word in words:
-        if GlobalStem.get_manager().filter(word=word).count() > 0:
+        stem = stemmer.stem(word)
+        qs = StemLabel.get_manager().filter(stem=stem)
+
+        if qs.count() == 0:
+            sl = StemLabel(label=word, stem=stem)
+            sl.save()
             continue
 
-        stem = stemmer.stem(word)
-        gs = GlobalStem(word=word, stem=stem)
-        gs.save()
+        assert(qs.count() == 1)
+
+        sl = qs[0]
+        if len(sl.label) < len(word):
+            continue
+
+        sl.label = word
+        sl.save()
 
 def stem(word):
-    qs = GlobalStem.get_manager().filter(word=word)
+    stem = stemmer.stem(word)
+    qs = StemLabel.get_manager().filter(stem=stem)
     if qs.count() == 0:
         return word
 
-    return qs[0].stem
+    return qs[0].label
