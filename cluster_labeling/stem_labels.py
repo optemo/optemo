550e28df5f31b21436aae093b9989e244aa59207
Nimalan Mahendran <nimalan@cs.ubc.ca>
2010-03-04 17:12:50 -0800

    Turning the WordStem table into a general-purpose table for Words

--- cluster_labeling/stem_labels.py
+++ cluster_labeling/stem_labels.py
@@ -17,24 +17,24 @@ class StemLabel(local.LocalModel):
     label = models.CharField(max_length=255, unique=True)
     stem = models.CharField(max_length=255, unique=True)
 
-class WordStem(local.LocalModel):
+class Word(local.LocalModel):
     class Meta:
-        db_table='word_stems'
+        db_table='words'
     
     word = models.CharField(max_length=255, unique=True)
     stem = models.CharField(max_length=255)
     count = models.BigIntegerField()
 
-def populate_word_stem_table():
-    WordStem.drop_table_if_exists()
-    WordStem.create_table()
+def populate_word_table():
+    Word.drop_table_if_exists()
+    Word.create_table()
 
     for review in optemo.CameraReview.get_manager():
         content = review.content
         words = th.get_words_from_string(content)
 
         for word in words:
-            word_qs = WordStem.get_manager().filter(word=word)
+            word_qs = Word.get_manager().filter(word=word)
 
             assert(word_qs.count() <= 1)
 
@@ -43,20 +43,20 @@ def populate_word_stem_table():
                 ws.count = F('count') + 1
             else:
                 stem = stemmer.stem(word)
-                ws = WordStem(word=word, stem=stem, count=1)
+                ws = Word(word=word, stem=stem, count=1)
             
             ws.save()
 
 def populate_stem_label_table():
-    populate_word_stem_table()
+    populate_word_table()
     
     StemLabel.drop_table_if_exists()
     StemLabel.create_table()
 
-    stem_qs = WordStem.get_manager().values('stem').distinct()
+    stem_qs = Word.get_manager().values('stem').distinct()
     for row in stem_qs:
         stem = row['stem']
-        label = WordStem.get_manager().filter(stem=stem).order_by('-count')[0].word
+        label = Word.get_manager().filter(stem=stem).order_by('-count')[0].word
         sl = StemLabel(stem=stem, label=label)
         sl.save()
 
