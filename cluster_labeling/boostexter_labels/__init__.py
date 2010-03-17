75a9df47cedcae8e17f21e98cc79e8e654ee5eff
Nimalan Mahendran <nimalan@cs.ubc.ca>
2010-03-16 17:11:44 -0700

    Adding needed import statements to toplevel portion of boostexter_labels

--- cluster_labeling/boostexter_labels/__init__.py
+++ cluster_labeling/boostexter_labels/__init__.py
@@ -1,19 +1,25 @@
+import cluster_labeling.optemo_django_models as optemo
+
+import cluster_labeling.boostexter_labels.training as training
+import cluster_labeling.boostexter_labels.combined_rules as combined_rules
+import cluster_labeling.boostexter_labels.labels as labels
+
 def train_boostexter_on_all_clusters\
         (version = optemo.CameraCluster.get_latest_version()):
     qs = optemo.CameraCluster.get_manager().filter(version=version)
     for cluster in qs:
-        generate_names_file(cluster)
-        generate_data_file(cluster)
-        train_boostexter(cluster)
+        training.generate_names_file(cluster)
+        training.generate_data_file(cluster)
+        training.train_boostexter(cluster)
         
 def make_boostexter_labels_for_all_clusters\
         (version = optemo.CameraCluster.get_latest_version()):
     qs = optemo.CameraCluster.get_manager().filter(version=version)
     for cluster in qs:
-        make_boostexter_labels_for_cluster(cluster)
+        labels.make_boostexter_labels_for_cluster(cluster)
         
 def save_combined_rules_for_all_clusters\
         (version = optemo.CameraCluster.get_latest_version()):
     qs = optemo.CameraCluster.get_manager().filter(version=version)
     for cluster in qs:
-        save_combined_rules_for_cluster(cluster)
+        combined_rules.save_combined_rules_for_cluster(cluster)
