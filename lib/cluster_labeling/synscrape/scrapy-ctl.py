7e7045fa560f6853e3880e4ba98cb50a04d953bc
Nimalan Mahendran <nimalan@cs.ubc.ca>
2010-03-31 12:50:09 -0700

    Scrapy-ctl has been modified to work with Django models and connect to the db

--- lib/cluster_labeling/synscrape/scrapy-ctl.py
+++ lib/cluster_labeling/synscrape/scrapy-ctl.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python
-
 import os
+import sys
+
 os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'synscrape.settings')
+
+try:
+    if len(sys.argv) < 4:
+        raise ValueError
+    
+    root_path = sys.argv[1]
+    config_name = sys.argv[2]
+except (IndexError, ValueError):
+    print "scrapy-ctl.py [root_path] [config_name] <other-scrapy-ctl-opts>"
+    sys.exit(-1)
+
+database_yaml_fn = os.path.join(root_path, "config/database.yml")
+    
+os.chdir(root_path)
+sys.path.append(os.path.join(root_path, "lib/"))
+
+import cluster_labeling.django_settings as django_settings
+django_settings.configure_django(database_yaml=database_yaml_fn,
+                                 config_name=config_name)
+
+sys.argv.pop(1)
+sys.argv.pop(1)
 
 from scrapy.command.cmdline import execute
 execute()
