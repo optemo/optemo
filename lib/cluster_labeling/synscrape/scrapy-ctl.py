9f32121e6b5a289e7a1a800cbcdba2e69a7add2d
Nimalan Mahendran <nimalan@cs.ubc.ca>
2010-04-15 13:55:56 -0700

    Scrapy now sets the Optemo product type
    - This is needed because otherwise the crawlers will not know which usecases to
      look at to find the list of words whose synonyms should be crawled.

--- lib/cluster_labeling/synscrape/scrapy-ctl.py
+++ lib/cluster_labeling/synscrape/scrapy-ctl.py
@@ -5,13 +5,14 @@ import sys
 os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'synscrape.settings')
 
 try:
-    if len(sys.argv) < 4:
+    if len(sys.argv) < 5:
         raise ValueError
     
     root_path = sys.argv[1]
     config_name = sys.argv[2]
+    product_type = sys.argv[3]
 except (IndexError, ValueError):
-    print "scrapy-ctl.py [root_path] [config_name] <other-scrapy-ctl-opts>"
+    print "scrapy-ctl.py [root_path] [config_name] [product_type] <other-scrapy-ctl-opts>"
     sys.exit(-1)
 
 database_yaml_fn = os.path.join(root_path, "config/database.yml")
@@ -23,8 +24,10 @@ import cluster_labeling.django_settings as django_settings
 django_settings.configure_django(database_yaml=database_yaml_fn,
                                  config_name=config_name)
 
-sys.argv.pop(1)
-sys.argv.pop(1)
+import cluster_labeling.optemo_django_models as optemo
+optemo.set_optemo_product_type(product_type)
+
+sys.argv = sys.argv[3:]
 
 from scrapy.command.cmdline import execute
 execute()
