2c40060880c5b020bc43e3badc35a5e4c45dba78
Nimalan Mahendran <nimalan@cs.ubc.ca>
2010-04-19 17:40:17 -0700

    Turned a bunch of 1-tuples into lists, otherwise they are treated as non-lists

--- lib/cluster_labeling/boostexter_labels/fields.py
+++ lib/cluster_labeling/boostexter_labels/fields.py
@@ -6,80 +6,80 @@ boosting_fields = \
 {
     optemo.Flooring :
     {'species' :
-     ('text',
+     ['text',
       {'text_to_btxtr_fn' : floor_fp.species_to_btxtr,
-       'btxtr_to_text_fn' : floor_fp.flooring_field_from_btxtr}),
+       'btxtr_to_text_fn' : floor_fp.flooring_field_from_btxtr}],
      'feature' :
-     ('text',
+     ['text',
       {'text_to_btxtr_fn' : floor_fp.feature_to_btxtr,
-       'btxtr_to_text_fn' : floor_fp.flooring_field_from_btxtr}),
+       'btxtr_to_text_fn' : floor_fp.flooring_field_from_btxtr}],
      'colorrange' :
-     ('text',
+     ['text',
       {'text_to_btxtr_fn' : floor_fp.colorrange_to_btxtr,
-       'btxtr_to_text_fn' : floor_fp.flooring_field_from_btxtr}),
+       'btxtr_to_text_fn' : floor_fp.flooring_field_from_btxtr}],
      'finish' :
-     ('text',
+     ['text',
       {'text_to_btxtr_fn' : floor_fp.finish_to_btxtr,
-       'btxtr_to_text_fn' : floor_fp.flooring_field_from_btxtr}),
+       'btxtr_to_text_fn' : floor_fp.flooring_field_from_btxtr}],
 
-     'width' : ('continuous'),
-     'thickness' : ('continuous'),
+     'width' : ['continuous'],
+     'thickness' : ['continuous'],
 
-     'price' : ('continuous'),
+     'price' : ['continuous'],
      'warranty' :
-     ('text',
+     ['text',
       {'text_to_btxtr_fn' : floor_fp.warranty_to_btxtr,
-       'btxtr_to_text_fn' : floor_fp.flooring_field_from_btxtr})},
+       'btxtr_to_text_fn' : floor_fp.flooring_field_from_btxtr}]},
     
     optemo.Printer :
-    {'itemweight' : ('continuous'),
+    {'itemweight' : ['continuous'],
 
-     'displaysize' : ('continuous'),
+     'displaysize' : ['continuous'],
 
-     'ppm' : ('continuous'),
-     'ppmcolor' : ('continuous'),
+     'ppm' : ['continuous'],
+     'ppmcolor' : ['continuous'],
 
      # These text fields are not easily usable because all punctuation
      # has to be removed from the field.
-     # 'duplex' : ('text'),
-     # 'papersize' : ('text'),
-     # 'special' : ('text'),
-     # 'platform' : ('text'),
+     # 'duplex' : ['text'],
+     # 'papersize' : ['text'],
+     # 'special' : ['text'],
+     # 'platform' : ['text'],
 
-     'resolutionarea' : ('continuous'),
+     'resolutionarea' : ['continuous'],
 
-     'paperinput' : ('continuous'),
-     'paperoutput' : ('continuous'),
+     'paperinput' : ['continuous'],
+     'paperoutput' : ['continuous'],
 
-     'colorprinter' : (['True', 'False']),
-     'scanner' : (['True', 'False']),
-     'printserver' : (['True', 'False']),
+     'colorprinter' : [['True', 'False']],
+     'scanner' : [['True', 'False']],
+     'printserver' : [['True', 'False']],
      
-     'averagereviewrating' : ('continuous'),
-     'totalreviews' : ('continuous'),
+     'averagereviewrating' : ['continuous'],
+     'totalreviews' : ['continuous'],
 
-     # 'price_ca' : ('continuous'),
-     'price' : ('continuous')},
+     # 'price_ca' : ['continuous'],
+     'price' : ['continuous']},
 
     
     optemo.Camera :
-    {'itemweight' : ('continuous'),
+    {'itemweight' : ['continuous'],
 
-     'displaysize' : ('continuous'),
+     'displaysize' : ['continuous'],
 
-     'opticalzoom' : ('continuous'),
-     'digitalzoom' : ('continuous'),
+     'opticalzoom' : ['continuous'],
+     'digitalzoom' : ['continuous'],
 
-     'maximumresolution' : ('continuous'),
+     'maximumresolution' : ['continuous'],
 
-     'maximumfocallength' : ('continuous'),
-     'minimumfocallength' : ('continuous'),
+     'maximumfocallength' : ['continuous'],
+     'minimumfocallength' : ['continuous'],
 
-     'averagereviewrating' : ('continuous'),
-     'totalreviews' : ('continuous'),
+     'averagereviewrating' : ['continuous'],
+     'totalreviews' : ['continuous'],
 
-     'price' : ('continuous'),
-     'price_ca' : ('continuous')}}
+     'price' : ['continuous'],
+     'price_ca' : ['continuous']}}
 
 fieldname_to_type = \
     dict(map(lambda (type, fields): (type, dict(fields)),
