f453aa977cacae26b4384fa734885fbc2d54e050
Nimalan Mahendran <nimalan@cs.ubc.ca>
2010-04-26 12:49:01 -0700

    Adding untested Ruby port of generate_data_file()

--- lib/cluster_labeling/boostexter_labels_rb/training.rb
+++ lib/cluster_labeling/boostexter_labels_rb/training.rb
@@ -13,7 +13,7 @@ module BtxtrLabels
     labels = get_labels(cluster)
     f.write(labels.map{|l| l.to_s()}.join(", "))
 
-    Boosting_fields[$DefaultProduct].each_pair\
+    Boosting_fields[$model].each_pair\
     { |fieldname, field|
       fielddesc = field[0]
 
@@ -34,9 +34,71 @@ module BtxtrLabels
   end
 
   def BtxtrLabels.default_text_to_btxtr_fn(text)
+    text.gsub!(/([-:,&]|#)/, ' ')
+    text.gsub!(/(\w+)\.(\D|$)/, "#{$1} #{$2}")
+    return text
   end
 
   def BtxtrLabels.generate_data_file(cluster)
+    filename = get_data_filename(cluster)
+    f = File.new(filename, "w")
+
+    version = cluster.version
+
+    products_this = cluster.nodes.map{|n| [n.product_id, cluster.id]}
+
+    parent_cluster_nodes = None
+
+    if cluster.parent_id == 0
+      clusters = $clustermodel.find(:all, :conditions => {:parent_id => 0, :version => version})
+      parent_cluster_nodes = clusters.map{|c| c.nodes}.flatten()
+    else
+      parent_cluster = $clustermodel.find(:all, :conditions => {:id => cluster.parent_id})[0]
+      parent_cluster_nodes = parent_cluster.nodes
+    end
+
+    products_parent = parent_cluster_nodes.map{|n| n.product}.find_all\
+    { |p|
+      # Get clusters for product and version
+      cluster_ids = \
+      $nodemodel.find(:all, :conditions => {:product_id => p.id, :version = version}).map\
+      { |n|
+        n.cluster_id
+      }
+
+      cluster_id_set = Set.new(cluster_ids)
+      not cluster_id_set.member?(cluster.id)
+    }
+    products_parent = products_parent.map{|p| [p.id, cluster.parent_id]}
+
+    products = products_this + products_parent
+
+    for product, cluster_id in products
+      Boosting_fields[$model].each_pair\
+      { |fieldname, field|
+        fielddesc = field[0]
+        fieldval = product.instance_variable_get(fieldname.to_sym())
+
+        if fielddesc = ['True', 'False']
+          if fieldval == '1' or fieldval == 'True'
+            fieldval = 'True'
+          else
+            fieldval = 'False'
+          end
+        elsif fieldval == None
+          fieldval = '?' # unknown value
+        elsif fielddesc = 'text'
+          if len(field) == 2 and field[1].has_key?('text_to_btxtr_fn')
+            fieldval = field[1]['text_to_btxtr_fn'](fieldval)
+          else
+            fieldval = default_text_to_btxtr_fn(fieldval)
+          end
+        end
+
+        f.write(fieldval)
+      }
+      f.write(cluster_id.to_s() + ".\n")
+    end
   end
 
   def BtxtrLabels.train_boostexter(cluster)
