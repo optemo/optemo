32ec27a501e88efc47b840f157312dd69360c8af
Nimalan Mahendran <nimalan@cs.ubc.ca>
2010-04-30 12:29:50 -0700

    Removed version from build_usecases because it is not used

--- lib/cluster_labeling/build_usecases.py
+++ lib/cluster_labeling/build_usecases.py
@@ -3,12 +3,11 @@ import os
 import sys
 
 try:
-    version = int(sys.argv[1])
-    root_path = sys.argv[2]
-    config_name = sys.argv[3]
-    product_type = sys.argv[4]
+    root_path = sys.argv[1]
+    config_name = sys.argv[2]
+    product_type = sys.argv[3]
 except (IndexError, ValueError):
-    print "build_usecases.py [version] [root_path] [config_name] [product_type]"
+    print "build_usecases.py [root_path] [config_name] [product_type]"
     sys.exit(-1)
 
 database_yaml_fn = os.path.join(root_path, "config/database.yml")
@@ -29,17 +28,17 @@ import subprocess
 usecases.populate_usecases()
 usecases.populate_direct_indicators_for_usecases()
 
-# import cluster_labeling.word_senses as ws
-# if not ws.WordSense.all_tables_exist()[0]:
-#     ws.WordSense.drop_tables_if_exist()
-#     ws.WordSense.create_tables()
+import cluster_labeling.word_senses as ws
+if not ws.WordSense.all_tables_exist()[0]:
+    ws.WordSense.drop_tables_if_exist()
+    ws.WordSense.create_tables()
 
-# cmd = "lib/cluster_labeling/synscrape/scrapy-ctl.py"
-# cmd = [os.path.join(root_path, cmd)]
-# cmd.extend(list(sys.argv[2:5]))
-# cmd.extend(['crawl', 'thesaurus.com'])
-# proc = subprocess.Popen(cmd)
-# retcode = proc.wait()
-# assert(retcode == 0)
+cmd = "lib/cluster_labeling/synscrape/scrapy-ctl.py"
+cmd = [os.path.join(root_path, cmd)]
+cmd.extend(list(sys.argv[1:4]))
+cmd.extend(['crawl', 'thesaurus.com'])
+proc = subprocess.Popen(cmd)
+retcode = proc.wait()
+assert(retcode == 0)
 
 usecases.populate_indicators_for_usecases()
