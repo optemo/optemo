b01cb4661d0ce51075204d07572b9b686a7dacad
Jan Ulrich <jan@jan-laptop.(none)>
2010-05-07 12:33:08 -0700

    Product valid only works for current product

--- app/models/cat_spec.rb
+++ app/models/cat_spec.rb
@@ -1,3 +1,11 @@
 class CatSpec < ActiveRecord::Base
   belongs_to :product
+  
+  def self.allSpecs(feat)
+    CachingMemcached.cache_lookup("#{$product_type}Cats-#{feat}") do
+      id_array = Product.valid.instock.map{|p| p.id }
+      ContSpec.cachemany(id_array, feat)
+    end
+  end
+
 end
