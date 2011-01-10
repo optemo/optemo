b83f8ca50fba60ca678d90c51a539f0f330193c9
Zev Thompson <zev@optemo.com>
2011-01-10 15:35:02 -0800

    Minor change to how rails server detects caching

--- app/models/caching_memcached.rb
+++ app/models/caching_memcached.rb
@@ -1,6 +1,8 @@
 # Be careful when selecting a hash key that you don't cache data with identical keys. This is the #1 source of caching bugs so far.
 module CachingMemcached
   def self.cache_lookup(key)
+    # There is another test that might work well, but it should be tested before being put into production
+    # if Rails.cache.class == ActiveSupport::Cache::MemCacheStore # We have memcache loaded
     unless Rails.env.development? || Rails.env.test?
       Rails.cache.fetch(key) { yield }
     else
