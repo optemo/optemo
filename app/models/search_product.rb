99a4cc35f5e46ab1a171913fa0d054637fab22ce
Maryam <maryam@maryam.(none)>
2011-07-13 12:48:01 -0700

    validation

--- app/models/search_product.rb
+++ app/models/search_product.rb
@@ -19,9 +19,9 @@ class SearchProduct < ActiveRecord::Base
           order =  "DESC"
            if s=='featured'
                mybins = [Userdatabin.new({:name => 'hero', :value => 1})]
-               res << search_id_q.create_join(mycats,mybins).conts_keywords.cats(mycats).bins(mybins).where("product_type=\'#{Session.product_type}\'")
+               res << search_id_q.create_join(mycats,mybins).conts_keywords.cats(mycats).bins(mybins)#.where("product_type=\'#{Session.product_type}\'")
                mybins = [Userdatabin.new({:name => 'featured', :value => 1})]
-               res << search_id_q.create_join(mycats,mybins).conts_keywords.cats(mycats).bins(mybins).where("product_type=\'#{Session.product_type}\'") 
+               res << search_id_q.create_join(mycats,mybins).conts_keywords.cats(mycats).bins(mybins)#.where("product_type=\'#{Session.product_type}\'") 
            else
                res << search_id_q.select("search_products.product_id, group_concat(cont_specs#{myconts.size}.name) AS names, group_concat(cont_specs#{myconts.size}.value) AS vals").create_join(mycats,mybins,myconts+[[],[]]).conts_keywords.cats(mycats).bins(mybins).where("cont_specs#{myconts.size+1}.name = '#{s}'").group("search_products.product_id").order("cont_specs#{myconts.size+1}.value #{order}")[0...18] 
            end      
