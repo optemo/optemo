f25a568e82e2665e56fa7d17b9cae6f8411cc66a
Zev Thompson <zev@Marias-iMac.(none)>
2011-08-03 17:22:47 -0700

    This should work for piwik id sensing

--- app/models/session.rb
+++ app/models/session.rb
@@ -52,10 +52,7 @@ class Session
     # Check for what Piwik site ID to put down in the optemo.html.erb layout
     # These site ids MUST match what's in the piwik database.
     p_type.urls.each do |u|
-      if request_url[u.url.to_s] 
-        p_url = u
-        break
-      end
+      p_url = u if request_url[u.url] 
     end
     p_url ||= p_type.urls.first
     self.piwikSiteId = p_url.piwik_id || 10 # This is a catch-all for testing sites.
