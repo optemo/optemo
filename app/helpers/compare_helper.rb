ef9d59e2f876dff27f5b19110afb9e94ee8e86c4
Jan Ulrich <jan@optemo.com>
2011-08-17 13:22:14 -0700

    Removed 'special' cases

--- app/helpers/compare_helper.rb
+++ app/helpers/compare_helper.rb
@@ -252,18 +252,7 @@ module CompareHelper
     res << '<span id="actioncount" style="display:none">' + "#{[Session.search.id.to_s].pack("m").chomp}</span>"
   	res
 	end
-  def navigator_bar_bottom_special(type)
-    prods = @s.search.products_landing(type)
-    type=="featured" ? s = prods.size : s=18
-    res = "<div id='navigator_bar_bottom'><div id='navtitle'><b>#{s.to_s + ' ' + navtitle}</b></div>#{link_to(t(Session.product_type+'.compare')+' (0) ', '#', {:class=>'awesome_reset_grey global_btn_grey nav-compare-btn', :id=>'nav_compare_btn_bottom'})}</div><div style='clear:both;'></div>"	    
-  end
-  
-  def navigator_bar_top_special(type)
-      prods = @s.search.products_landing(type)
-      type=="featured" ? s = prods.size : s=18
-      res = "<div id='navigator_bar'>
- 	  <div id='navtitle'><div class='nav-number'>#{s}</div>"
-  end      
+    
   def navigator_bar_bottom 
     res = "<div id='navigator_bar_bottom'><div id='navtitle'>#{Session.search.products_size.to_s + ' ' + navtitle}</div>#{link_to(t(Session.product_type+'.compare')+' (0) ', '#', {:class=>'awesome_reset_grey global_btn_grey nav-compare-btn', :id=>'nav_compare_btn_bottom'})}</div><div style='clear:both;'></div>"
   
