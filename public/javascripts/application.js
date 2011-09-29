2f4819b0b54f2cd42d83f3e11cf0fe683db55930
Jan Ulrich <jan@optemo.com>
2011-09-29 12:58:45 -0700

    Swatches have been split between styling and click handler

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -805,7 +805,7 @@ optemo_module = (function (my){
         //});
 
         // Add a color selection -- submit
-        $('.swatch').live('click', function(){
+        $('.swatch_button').live('click', function(){
             if (my.loading_indicator_state.disable) return false;
             var t = $(this);
             var whichThingSelected = t.attr("style").replace(/background-color: (\w+);?/i,'$1');
