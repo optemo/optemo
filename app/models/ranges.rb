f341cb40c7b533737e75526f2db767714857ee03
Maryam <maryam@maryam.(none)>
2012-03-22 17:45:56 -0700

    fixed price ranges

--- app/models/ranges.rb
+++ app/models/ranges.rb
@@ -1,17 +1,18 @@
 module Ranges
   
 	def self.getRange(feat, num)
-	  discretized = Session.search.solr_search({mybins: [], mycats: [], myconts: []}).facet(feat.to_sym).rows
+	  @discretized ||= Session.search.solr_search({mybins: [], mycats: [], myconts: []}).facet(feat.to_sym).rows
 	  rs = []
-	  if (!discretized.empty?)
-	    min_all = CachingMemcached.cache_lookup("Min#{Session.search.keyword_search}#{feat}") {discretized.first.value}
-	    max_all = CachingMemcached.cache_lookup("Max#{Session.search.keyword_search}#{feat}") {discretized.last.value}
+	  if (!@discretized.empty?)
+	    min_all = CachingMemcached.cache_lookup("Min#{Session.search.keyword_search}#{feat}") {@discretized.first.value}
+	    max_all = CachingMemcached.cache_lookup("Max#{Session.search.keyword_search}#{feat}") {@discretized.last.value}
 	    if feat=="saleprice"
-	      p_min = discretized.first.value
-	      p_max = discretized.last.value
+	      p_min = @discretized.first.value
+	      p_max = @discretized.last.value
 	      rs  = self.price_ranges(p_min, p_max)
       else    
-	      grouped_data = Kmeans.compute(num, discretized.map{|r| [r.value]*r.count}.flatten)
+	      grouped_data = Kmeans.compute(num, @discretized.map{|r| [r.value]*r.count}.flatten)
+	      #debugger
 	      debugger if grouped_data.nil?
         #grouped_data.each do  |g|
         #  puts "#{g.min}-#{g.max} (#{g.count.to_s})"
@@ -26,30 +27,31 @@ module Ranges
 	end
 	
 	def self.count(feat, min, max)
-	  # this is a hack, should be rewriten
-	  #temp = Session.search.userdataconts
-    #Session.search.userdataconts = []
-	  #a = Session.search.solr_cached.facet(feat.to_sym).rows.map{|r| [r.value]*r.count}.flatten
 	  c = Session.search.solr_cached.facet(feat.to_sym).rows.select{|p| p.value == min||(p.value<max && p.value>=min) }.inject(0){|sum,elem|sum+elem.count}  
 	  #Session.search.userdataconts = temp
 	  c
 	end
 	  
   def self.cacherange(feat, num) 
-     Rails.cache.fetch("Ranges#{feat}#{num}") do
+    # Rails.cache.fetch("Ranges#{feat}#{num}") do
        self.getRange(feat, num)
-     end
+    # end
   end
 
  def self.price_ranges(min, max)
-   prs = [0,50,100, 150, 175, 200, 300, 500, 1000, 2000, 3000, 5000, 1000000]
+   prs = [0, 50, 100, 150, 200, 300, 500, 1000, 2000, 3000, 5000, 1000000]
    rs = []
-   prs.each_with_index do |pr, ind| 
-     if rs.empty? && pr>min 
-       rs << {:min => prs[ind-1], :max => pr}
-     elsif !rs.empty?
-       rs << {:min => prs[ind-1], :max => pr}
-     end 
+   prs.each_with_index do |pr, ind|
+     if max<prs.last
+       if rs.empty? && pr>min 
+         rs << {:min => prs[ind-1], :max => pr}
+       elsif !rs.empty? && max>pr 
+         rs << {:min => prs[ind-1], :max => pr}
+         if max>=pr && max<prs[ind+1]
+           rs << {:min => pr, :max => prs[ind+1]}
+         end 
+       end
+    end
    end
    rs
  end
