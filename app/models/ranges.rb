37a805cd4e312b579eaa3064a2fc00e8e7250d02
Maryam <maryam@maryam.(none)>
2012-04-03 11:52:07 -0700

    caching discretized vals

--- app/models/ranges.rb
+++ app/models/ranges.rb
@@ -1,13 +1,11 @@
 module Ranges
   
 	def self.getRange(feat, num)
-	  discretized = Session.search.solr_search({mybins: [], mycats: [], myconts: []}).facet(feat.to_sym).rows
+	  discretized = self.getRows(feat) 
 	  rs = []
 	  if (!discretized.empty?)
 	    if feat=="saleprice"
-	      p_min = discretized.map{|d| d.value}.min
-	      p_max = discretized.map{|d| d.value}.max
-	      rs  = self.price_ranges(p_min, p_max)
+	      rs  = self.price_ranges(discretized)
       else    
 	      grouped_data = Kmeans.compute(num, discretized.map{|r| [r.value]*r.count}.flatten)
 	      #debugger
@@ -23,6 +21,11 @@ module Ranges
     rs
   end
   
+  def self.getRows(feat)
+     @rows ||= Session.search.solr_search({mybins: [], mycats: [], myconts: []})
+     @rows.facet(feat.to_sym).rows
+  end
+    
   def self.count(feat, min, max)
     if (feat == "saleprice")
       Session.search.solr_cached.facet(feat.to_sym).rows.select{|p| p.value == min||(p.value<max && p.value>=min) }.inject(0){|sum,elem|sum+elem.count}
@@ -37,15 +40,22 @@ module Ranges
     end
   end
 
-  def self.price_ranges(min, max)
+  def self.price_ranges(discretized)
+    min = discretized.map{|d| d.value}.min
+    max = discretized.map{|d| d.value}.max
+    s = self.count(saleprice, min, max) # total count of prods with this feature
     prs = [0, 50, 100, 150, 200, 300, 500, 1000, 2000, 3000, 5000, 1000000]
-    rs = []
+    gprs = [0, 10, 25, 50]
     prs.each_with_index do |pr, ind|
       if max<prs.last
-        if rs.empty? && pr>min 
-          rs << {:min => prs[ind-1], :max => pr}
+        if rs.empty? && pr>min
+          if self.count(saleprice, prs[ind-1], pr) > s/4
+            
+          else
+            rs << {:min => prs[ind-1], :max => pr}
+          end      
         elsif !rs.empty? && max>pr 
-          rs << {:min => prs[ind-1], :max => pr}
+          rs << {:min => prs[ind-1], :max => pr} 
           if max>=pr && max<prs[ind+1]
             rs << {:min => pr, :max => prs[ind+1]}
           end 
