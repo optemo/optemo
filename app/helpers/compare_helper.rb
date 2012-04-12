1fa7fe505915ebf45b26a3989d03320461e24d36
Marc Lejay <marc.lejay@hotmail.com>
2012-04-12 10:54:25 -0700

    Resolve merge conflicts

--- app/helpers/compare_helper.rb
+++ app/helpers/compare_helper.rb
@@ -80,11 +80,11 @@ module CompareHelper
       dr << {:count => Ranges.count(feat, r[:min], r[:max]), :min => r[:min], :max => r[:max], :display => ""}
       if dr.last[:count] >0 
         if r[:min] == r[:max] 
-         if feat == "saleprice" && I18n.locale == :en
-           dis = "$#{r[:min]}"
-         else
-           dis =  "#{r[:min]} " + t("#{Session.product_type}.filter.#{feat}.unit") 
-         end   
+          if feat == "saleprice" && I18n.locale == :en
+            dis = "$#{r[:min]}"
+          else
+            dis =  "#{r[:min]} " + t("#{Session.product_type}.filter.#{feat}.unit") 
+          end   
         else
           if feat == "saleprice" && I18n.locale == :en
             dis = "$#{r[:min]} - $#{r[:max]}"
@@ -92,8 +92,8 @@ module CompareHelper
             dis = "#{r[:min]} "+t("#{Session.product_type}.filter.#{feat}.unit")  +" - #{r[:max]} " + t("#{Session.product_type}.filter.#{feat}.unit")
           end    
         end 
-        dr.last[:display] << dis  
-      end  
+        dr.last[:display] << dis
+      end
     end   
     dr = dr.map{|d| d if d[:count]>0}.compact
     if feat == "saleprice" && I18n.locale == :en
