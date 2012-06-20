91ce995514760ae8e86a542c7203d61d7a43e709
Jan Ulrich <jan@optemo.com>
2012-06-20 09:06:55 -0700

    Another tweak to the selector

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 set :stages, %w(sbox production linode slicehost sandbox fremont uniserve)
+set :rvm_type, :system
 set :rvm_ruby_string, '1.9.3'
 require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
