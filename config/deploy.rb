2be941f8e0b8f4309457a5b4c810874ecfec8257
Jan Ulrich <jan@optemo.com>
2012-06-19 18:51:57 -0700

    Updated rvm integration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,6 @@
 set :stages, %w(sbox production linode slicehost sandbox fremont uniserve)
+set :rvm_ruby_string, '1.9.3'
 require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
-# The next two lines are needed for integration with Ruby Version Manager:
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-set :rvm_ruby_string, '1.9.3'
+
